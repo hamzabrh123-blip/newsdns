f2099e72b04d4710febfc77468ce3f6d37d0834a
Hamza <hamzabrh123@gmail.com>
2025-12-02 00:00:07 +0530

    Auto superuser creation in wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,3 +14,28 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+# AUTO CREATE SUPERUSER ON RENDER
+import os
+from django.contrib.auth import get_user_model
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
+
+try:
+    User = get_user_model()
+    username = "adminauto"
+    email = "admin@example.com"
+    password = "Admin@12345"
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superuser created automatically!")
+except Exception as e:
+    print("Superuser creation failed:", str(e))
