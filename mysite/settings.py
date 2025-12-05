769998853299661263af2dd22990acf4cf4f0d2d
Hamza <hamzabrh123@gmail.com>
2025-12-05 23:04:10 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +84,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # ---------------- DATABASE ----------------
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgresql://brhnewsdb_user:Wg0XSw1GablPeCkybLFZ1wQ47CfW67M1@dpg-d4oqla7diees73dpqq60-a/brhnewsdb",
+        conn_max_age=600
+    )
 }
 
 # ---------------- PASSWORD VALIDATION ----------------
