a7e0b6401bb9d86d9cfa9c4dde626b89c942779a
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-08 01:15:36 +0530

    Update models.py

--- mynews/models.py
+++ mynews/models.py
@@ -5,6 +5,8 @@ class District(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
 class News(models.Model):
+    image = models.ImageField(upload_to='news_image') 
+class News(models.Model):
     CATEGORY_CHOICES = [
         ('International', 'International'),
         ('National', 'National'),
