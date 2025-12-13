2c656de1c7d204d42f69db1dc1dbef091d48590d
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-13 22:43:09 +0530

    Add ads.txt view to serve ads.txt content
    Added a view for serving ads.txt file.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
-from django.urls import path, include        # ← IMPORTANT (path यहीं से आता है)
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from mynews import views
 from mynews.views import create_admin
 
+
+# ✅ ads.txt view
+def ads_txt(request):
+    content = render_to_string("ads.txt")
+    return HttpResponse(content, content_type="text/plain")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # ✅ ads.txt ROOT par
+    path('ads.txt', ads_txt),
+
     path('', include('mynews.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
