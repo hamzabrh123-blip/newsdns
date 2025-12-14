3d7650c60f0cdf85887c42ab6782630e10311118
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-14 10:22:26 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,30 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
 from django.template.loader import render_to_string
-
 from mynews import views
 from mynews.views import create_admin
 
-
-# ✅ ads.txt view
-def ads_txt(request):
-    content = render_to_string("ads.txt")
-    return HttpResponse(content, content_type="text/plain")
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # ✅ ads.txt ROOT par
-    path('ads.txt', ads_txt),
-
     path('', include('mynews.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
-# Extra admin URLs
 urlpatterns += [
     path("create-admin/", create_admin),
     path("admin-login/", views.admin_login, name="admin_login"),
