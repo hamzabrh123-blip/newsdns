ed6adb5e9cb6834ed644b46c420497a881bc11d8
Hamza <hamzabrh123@gmail.com>
2025-12-15 00:31:12 +0530

    Bypass failed AlterField migrations 0016 & 0017 via fake

---
+++ mynews/migrations/0020_fake_forward_slug_fix.py
@@ -0,0 +1,11 @@
+
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mynews', '0015_news_slug_alter_news_image'), 
+    ]
+
+    operations = [
+    ]
