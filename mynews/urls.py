1af61fdde4c9c7e53eb3d040553a26ac50111f1d
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-15 21:36:50 +0530

    Update urls.py

--- mynews/urls.py
+++ mynews/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('national/', views.national_news, name='national_news'),
-    path('news/<int:news_id>/', views.news_detail, name='news_detail'),
+    path('news/<slug:slug>-<int:news_id>/', views.news_detail, name='news_detail'),
+    #path('news/<int:news_id>/', views.news_detail, name='news_detail'),
     path('district/<str:district>/', views.district_news, name='district_news'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
