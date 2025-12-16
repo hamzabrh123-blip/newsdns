70051f0830fff9192824be03d26e30e0d1790b91
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-16 17:40:31 +0530

    Update urls.py

--- mynews/urls.py
+++ mynews/urls.py
@@ -2,17 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('national/', views.national_news, name='national_news'),
-    path(
-    "news/<slug:slug>-<str:code>/",
-    views.news_detail,
-    name="news_detail"
-),
-    path('district/<str:district>/', views.district_news, name='district_news'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path("", views.home, name="home"),
+    path("national/", views.national_news, name="national_news"),
 
-    # ✅ ads.txt (IMPORTANT – root level)
-    path('ads.txt', views.ads_txt, name='ads_txt'),
+    # SEO + Secure URL
+    path(
+        "news/<slug:slug>-<str:code>/",
+        views.news_detail,
+        name="news_detail"
+    ),
+
+    path("district/<str:district>/", views.district_news, name="district_news"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+
+    # ads.txt at root
+    path("ads.txt", views.ads_txt, name="ads_txt"),
 ]
