6f7f7bc2efee68e187318d1dff2f7ee0991ea643
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-17 20:41:58 +0530

    Update urls.py

--- mynews/urls.py
+++ mynews/urls.py
@@ -6,21 +6,18 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("national/", views.national_news, name="national_news"),
 
-<<<<<<< HEAD
     # SEO + Secure URL
     path(
         "news/<slug:slug>-<str:code>/",
         views.news_detail,
         name="news_detail"
     ),
-=======
+
     # ✅ Add news page
     path("add-news/", views.add_news, name="add_news"),
 
     # ✅ SEO Friendly News URL (ID hata diya)
     re_path(r'^news/(?P<slug>[-\w\d\u0900-\u097F]+)/$', views.news_detail, name='news_detail'),
->>>>>>> eb7583f (Initial commit: fixed slug and template errors)
-
     path("district/<str:district>/", views.district_news, name="district_news"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
