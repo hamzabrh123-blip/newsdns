cf14c95106d8cb4048d40e17da1a174c3ce63cb2
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-25 10:00:31 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,18 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from mynews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mynews.urls')),  # Main app URLs
+    path('', include('mynews.urls')),  # Main app urls
     path('ckeditor/', include('ckeditor_uploader.urls')),
-
-    # Custom admin paths
-    path('admin-login/', views.admin_login, name='admin_login'),
-    path('admin-verify/', views.admin_verify, name='admin_verify'),
 ]
 
-# Serve media files during DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
