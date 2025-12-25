6980d15c61cb585411d702d07b5796f5444f4ad5
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-25 08:28:47 +0530

    Update context_processors.py

--- mynews/context_processors.py
+++ mynews/context_processors.py
@@ -1,4 +1,5 @@
 from .models import News
+from django.core.cache import cache
 
 def important_news(request):
     return {
@@ -6,3 +7,7 @@ def important_news(request):
             is_important=True
         ).order_by('-date')[:20]
     }
+
+def site_visits(request):
+    visits = cache.get("visits", 0)
+    return {"visits": visits}
