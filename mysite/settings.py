cd1dd801cb1ae37931790499eff4f7838a4fb24b
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-25 08:30:58 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'mynews.context_processors.important_news',
+                "mynews.context_processors.site_visits",
             ],
         },
     },
