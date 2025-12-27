2e7d519823b7a9f272bd158069fe5990d1f38b1f
hamzabrh123-blip <hamzabrh123@gmail.com>
2025-12-27 14:20:55 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def google_verify(request):
+    return HttpResponse(
+        "google-site-verification: google21a82f00fad0f9b3.html",
+        content_type="text/html"
+    )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        "google21a82f00fad0f9b3.html",
+        google_verify
+    ),
+
     path('', include('mynews.urls')),  # Main app urls
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
