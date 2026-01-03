d0af47759d436d9e68ebc0583ef09ee08ba3cca2
hamzabrh123-blip <hamzabrh123@gmail.com>
2026-01-03 09:13:52 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ---------------- SECURITY ----------------
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-key")
-DEBUG = True
+DEBUG = True # Live hone par ise False kar dena
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -31,14 +31,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    # Your app
     "mynews",
-
-    # Rich text editor
     "ckeditor",
     "ckeditor_uploader",
-
-    # Cloudinary
     "cloudinary",
 ]
 
@@ -46,7 +41,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -69,8 +63,9 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "mynews.context_processors.important_news",
-                "mynews.context_processors.site_visits",
+                # Temporary hata diya hai crash rokne ke liye
+                # "mynews.context_processors.important_news",
+                # "mynews.context_processors.site_visits",
             ],
         },
     },
@@ -79,8 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "mysite.wsgi.application"
 
 # ---------------- DATABASE ----------------
-# Make sure DATABASE_URL is set in environment
-# Example format: postgres://username:password@host:port/dbname
 DATABASES = {
    "default": dj_database_url.parse(
         "postgres://brhnewsdb_user:Wg0XSw1GablPeCkybLFZ1wQ47CfW67M1@dpg-d4oqla7diees73dpqq60-a.oregon-postgres.render.com/brhnewsdb",
@@ -89,28 +82,19 @@ DATABASES = {
     )
 }
 
-# ---------------- AUTH PASSWORD VALIDATORS ----------------
-AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-]
-
 # ---------------- GENERAL ----------------
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kolkata" # India time set kiya hai
 USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# ---------------- STATIC FILES ----------------
+# ---------------- STATIC & MEDIA ----------------
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# ---------------- MEDIA (CLOUDINARY) ----------------
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": os.environ.get("CLOUDINARY_NAME", "dvoqsrkkq"),
     "API_KEY": os.environ.get("CLOUDINARY_KEY", "468226887694915"),
@@ -125,21 +109,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "hamzabrh@gmail.com")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "brjboidzxxciayvw")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # ---------------- CKEDITOR ----------------
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_STORAGE_BACKEND = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
-# ---------------- LOGGING (Optional) ----------------
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "INFO",
-    },
-}
