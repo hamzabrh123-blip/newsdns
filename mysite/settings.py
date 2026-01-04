6632b3f5b7bd8896d403fbd3eb2ed92428aefdd9
Hamza <hamzabrh123@gmail.com>
2026-01-04 11:03:56 +0530

    Final SSL Fix after pull

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=0,  # Isse timeout ke chances kam ho jate hain
+        conn_max_age=0,
     )
 }
 
