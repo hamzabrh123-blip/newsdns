e622fb0f08370df56c0eeb91a48ae076b89ed512
hamzabrh123-blip <hamzabrh123@gmail.com>
2026-01-04 10:06:57 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
+        conn_params={'sslmode': 'require'}  # 👈 Ye line SSL error theek karegi
     )
 }
 
