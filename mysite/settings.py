34c53507cc83488b0de54ef9dfb4b2aa265de507
Hamza <hamzabrh123@gmail.com>
2026-01-04 17:49:03 +0530

    Fixed indentation error in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "mysite.wsgi.application"
 
 # ---------------- DATABASE ----------------
- DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600, # Connection ko thoda hold karne ke liye
