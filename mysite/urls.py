1616209a876f4db84d85443d626fa0f65c58d956
hamzabrh123-blip <hamzabrh123@gmail.com>
2026-01-17 11:55:17 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,13 +16,11 @@ urlpatterns = [
     # Google Verification
     path("google21a82f00fad0f9b3.html", google_verify),
 
-    # CKEditor Uploader (यही आपकी फोटो अपलोड कराता है)
+    # CKEditor Uploader 
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
-    # Main Website URLs (इसे नीचे ही रखें ताकि बाकी paths पहले चेक हों)
     path('', include('mynews.urls')),  
 ]
 
-# Media और Static files के लिए (Production और Local दोनों में बेहतर काम करेगा)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
