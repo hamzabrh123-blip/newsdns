7316c9d4377411073ba6c306b7deb639b8d2aede
Hamza <hamzabrh123@gmail.com>
2026-02-03 03:02:08 +0530

    Fixed missing image_url field in admin fieldsets

--- mynews/admin.py
+++ mynews/admin.py
@@ -8,11 +8,13 @@ class NewsAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
     list_editable = ('is_important', 'share_now_to_fb')
     list_per_page = 20
+    # image_url ko readonly mat rakhna taaki tum link edit kar sako agar zaroorat pade
     readonly_fields = ('is_fb_posted', 'slug')
 
     fieldsets = (
         ('News Details', {
-            'fields': ('title', 'content', 'category', 'district', 'url_city', 'image', 'youtube_url')
+            # Yahan maine 'image_url' add kar diya hai
+            'fields': ('title', 'content', 'category', 'district', 'url_city', 'image', 'image_url', 'youtube_url')
         }),
         ('Facebook Control', {
             'fields': ('share_now_to_fb', 'is_fb_posted'),
