a286d1a7d81555079aed87c217bd41a096e2e3a8
hamzabrh123-blip <hamzabrh123@gmail.com>
2026-02-02 22:39:04 +0530

    Update admin.py

--- mynews/admin.py
+++ mynews/admin.py
@@ -3,21 +3,9 @@ from .models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    # 1. Ye saare columns aapko admin panel mein saamne dikhenge
-    # 'url_city' ko 'district' ke baad rakha hai taaki asani ho
     list_display = ('title', 'district', 'category', 'date', 'is_important')
-    
-    # 2. Side mein filter (District ab Dropdown hai toh filter mast kaam karega)
-    list_filter = ('district', 'category', 'date')
-    
-    # 3. Search bar: Title aur Content se news dhoondne ke liye
+    list_filter = ('district', 'category', 'date', 'is_important')
     search_fields = ('title', 'content')
-    
-    # 4. News likhte waqt Slug apne aap ban jaye (Auto-fill)
     prepopulated_fields = {'slug': ('title',)}
-    
-    # 5. Ek page par kitni news dikhe
     list_per_page = 20
-
-# 'admin.site.register(District)' ko maine hata diya hai 
-# kyunki ab District model exist nahi karta.
+    list_editable = ('is_important',)
