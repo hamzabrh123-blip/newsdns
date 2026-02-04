984d951d1e8b8c28ecf5dc9e8a7c01a316c3313d
hamzabrh123-blip <hamzabrh123@gmail.com>
2026-02-04 19:59:43 +0530

    Update admin.py

--- mynews/admin.py
+++ mynews/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.db import models  # Field overrides ke liye zaroori hai
+from django.db import models 
 from .models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    # List view jahan saari news dikhti hain
     list_display = ('title', 'district', 'category', 'date', 'is_important', 'share_now_to_fb', 'is_fb_posted')
     list_filter = ('district', 'category', 'date', 'is_important', 'is_fb_posted')
     search_fields = ('title', 'content')
@@ -12,7 +11,6 @@ class NewsAdmin(admin.ModelAdmin):
     list_per_page = 20
     readonly_fields = ('is_fb_posted', 'slug')
 
-    # --- TITLE BOX KO BROAD (CHAUDA) KARNE KA LOGIC ---
     formfield_overrides = {
         models.CharField: {
             'widget': admin.widgets.AdminTextInputWidget(
@@ -23,7 +21,6 @@ class NewsAdmin(admin.ModelAdmin):
         },
     }
 
-    # Admin Edit Page ka Structure
     fieldsets = (
         ('Headline Section (Broad View)', {
             'fields': ('title',),
@@ -34,7 +31,7 @@ class NewsAdmin(admin.ModelAdmin):
         }),
         ('Big Controller (Category & Location)', {
             'fields': (('category', 'district', 'url_city'),),
-            'description': 'Select karein ki news kahan dikhani hai (Middle East, Sports, etc.)'
+            'description': 'Select karein ki news kahan dikhani hai.'
         }),
         ('Media Section', {
             'fields': (('image', 'image_url'), 'youtube_url'),
@@ -44,10 +41,10 @@ class NewsAdmin(admin.ModelAdmin):
         }),
         ('Advanced SEO & Settings', {
             'fields': ('meta_keywords', 'slug', 'is_important', 'date'),
-            'classes': ('collapse',), # Is par click karne par hi khulega
+            'classes': ('collapse',),
         }),
     )
 
-    # Automatically date fill karne ke liye agar blank chhoda
+    # FINAL FIX: super().save_model ko hataya hai taaki Cloudinary dubara file na maange
     def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
+        obj.save()
