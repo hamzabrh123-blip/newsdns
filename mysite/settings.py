5d20c65fad09e4d3bcb4832ee449a1b3e5eaeede
hamzabrh123-blip <hamzabrh123@gmail.com>
2026-02-09 15:28:10 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,3 +133,10 @@ FB_GROUP_2_ID = os.environ.get("FB_GROUP_2_ID")
 IMGBB_API_KEY = os.environ.get("IMGBB_API_KEY")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# --- INTERNATIONALIZATION ---
+# यह हिस्सा समय और भाषा को इंडिया के हिसाब से सेट करेगा
+LANGUAGE_CODE = "hi"
+TIME_ZONE = "Asia/Kolkata"
+USE_I18N = True
+USE_TZ = True
