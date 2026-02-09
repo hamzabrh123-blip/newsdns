7a5fb9ea9e8c76d11dd75240736f7159e7061144
hamzabrh123-blip <hamzabrh123@gmail.com>
2026-02-10 02:01:54 +0530

    Create constants.py

---
+++ mynews/constants.py
@@ -0,0 +1,28 @@
+# mynews/constants.py
+
+UP_DISTRICTS = [
+    ('Agra', 'आगरा', 'agra'), ('Aligarh', 'अलीगढ़', 'aligarh'), ('Ambedkar-Nagar', 'अम्बेडकर नगर', 'ambedkar-nagar'), ('Amethi', 'अमेठी', 'amethi'), ('Amroha', 'अमरोहा', 'amroha'),
+    ('Auraiya', 'औरैया', 'auraiya'), ('Ayodhya', 'अयोध्या', 'ayodhya'), ('Azamgarh', 'आजमगढ़', 'azamgarh'), ('Baghpat', 'बागपत', 'baghpat'), ('Bahraich', 'बहराइच', 'bahraich'), 
+    ('Ballia', 'बलिया', 'ballia'), ('Balrampur', 'बालरामपुर', 'balrampur'), ('Banda', 'बांदा', 'banda'), ('Barabanki', 'बाराबंकी', 'barabanki'), ('Bareilly', 'बरेली', 'bareilly'), 
+    ('Basti', 'बस्ती', 'basti'), ('Bhadohi', 'भदोही', 'bhadohi'), ('Bijnor', 'बिजनौर', 'bijnor'), ('Budaun', 'बदायूँ', 'budaun'), ('Bulandshahr', 'बुलंदशहर', 'bulandshahr'),
+    ('Chandauli', 'चंदौली', 'chandauli'), ('Chitrakoot', 'चित्रकूट', 'chitrakoot'), ('Deoria', 'देवरिया', 'deoria'), ('Etah', 'एटा', 'etah'), ('Etawah', 'इटावा', 'etawah'),
+    ('Farrukhabad', 'फर्रुखाबाद', 'farrukhabad'), ('Fatehpur', 'फतेहपुर', 'fatehpur'), ('Firozabad', 'फिरोजाबाद', 'firozabad'), ('Gautam-Buddha-Nagar', 'नोएडा', 'gautam-buddha-nagar'), 
+    ('Ghaziabad', 'गाजियाबाद', 'ghaziabad'), ('Ghazipur', 'गाजीपुर', 'ghazipur'), ('Gonda', 'गोंडा', 'gonda'), ('Gorakhpur', 'गोरखपुर', 'gorakhpur'), ('Hamirpur', 'हमीरपुर', 'hamirpur'), 
+    ('Hapur', 'हापुड़', 'hapur'), ('Hardoi', 'हरदोई', 'hardoi'), ('Hathras', 'हाथरास', 'hathras'), ('Jalaun', 'जालौन', 'jalaun'), ('Jaunpur', 'जौनपुर', 'jaunpur'), 
+    ('Jhansi', 'झाँसी', 'jhansi'), ('Kannauj', 'कन्नौज', 'kannauj'), ('Kanpur-Dehat', 'कानपुर देहात', 'kanpur-dehat'), ('Kanpur-Nagar', 'कानपुर नगर', 'kanpur-nagar'), 
+    ('Kasganj', 'कासगंज', 'kasganj'), ('Kaushambi', 'कौशाम्बी', 'kaushambi'), ('Kushinagar', 'कुशीनगर', 'kushinagar'), ('Lakhimpur-Kheri', 'लखीमपुर खीरी', 'lakhimpur-kheri'), 
+    ('Lalitpur', 'ललितपुर', 'lalitpur'), ('Lucknow', 'लखनऊ', 'lucknow'), ('Maharajganj', 'महराजगंज', 'maharajganj'), ('Mahoba', 'महोबा', 'mahoba'), ('Mainpuri', 'मैनपुरी', 'mainpuri'), 
+    ('Mathura', 'मथुरा', 'mathura'), ('Mau', 'मऊ', 'mau'), ('Meerut', 'मेरठ', 'meerut'), ('Mirzapur', 'मिर्जापुर', 'mirzapur'), ('Moradabad', 'मुरादाबाद', 'moradabad'), 
+    ('Muzaffarnagar', 'मुजफ्फरनगर', 'muzaffarnagar'), ('Pilibhit', 'पीलीभीत', 'pilibhit'), ('Pratapgarh', 'प्रतापगढ़', 'pratapgarh'), ('Prayagraj', 'प्रयागराज', 'prayagraj'), 
+    ('Rae-Bareli', 'रायबरेली', 'rae-bareli'), ('Rampur', 'रामपुर', 'rampur'), ('Saharanpur', 'सहारनपुर', 'saharanpur'), ('Sambhal', 'सम्भल', 'sambhal'), 
+    ('Sant-Kabir-Nagar', 'संत कबीर नगर', 'sant-kabir-nagar'), ('Shahjahanpur', 'शाहजहांपुर', 'shahjahanpur'), ('Shamli', 'शामली', 'shamli'), ('Shravasti', 'श्रावस्ती', 'shravasti'), 
+    ('Siddharthnagar', 'सिद्धार्थनगर', 'siddharthnagar'), ('Sitapur', 'सीतापुर', 'sitapur'), ('Sonbhadra', 'सोनभद्र', 'sonbhadra'), ('Sultanpur', 'सुलतानपुर', 'sultanpur'), 
+    ('Unnao', 'उन्नाव', 'unnao'), ('Varanasi', 'वाराणसी', 'varanasi'),
+]
+
+OTHER_CATEGORIES = [
+    ('Delhi', 'दिल्ली', 'delhi'), ('Other-State', 'अन्य राज्य', 'other-state'), ('National', 'राष्ट्रीय खबर', 'national'), ('International', 'अंतर्राष्ट्रीय', 'international'), 
+    ('Sports', 'खेल समाचार', 'sports'),('Bollywood', 'बॉलीवुड', 'bollywood'), ('Hollywood', 'हॉलीवुड', 'hollywood'), ('Technology', 'टेक्नोलॉजी', 'technology'), ('Market', 'मार्केट भाव', 'market'),
+]
+
+LOCATION_DATA = UP_DISTRICTS + OTHER_CATEGORIES
