d16804d2c78f9d20564b7c0b208ac7830a00100a
Hamza <hamzabrh123@gmail.com>
2026-07-16 20:43:31 +0530

    Updated redirects.py: changed 404 to 410 for better SEO/status handling

--- shopping/redirects.py
+++ shopping/redirects.py
@@ -4,7 +4,6 @@ from .views import gone_view
 
 # Sirf SEO/Redirects wale patterns
 seo_urlpatterns = [
-    re_path(r'^(prayagraj|ujjain|deoria|ghaziabad|technology|other-state|shopping|mahoba|agra|hyderabad|international|news|market|gonda|amethi|bahraich|bijnor|bollywood|Basti|Farrukhabad|kanpur-nagar|Kanpur-Dehat|gorakhpur|district|new-delhi|goa|mumbai|lucknow|national|google|delhi|kannauj|market-news|sports|varanasi|toronto-canada|mathura|mainpuri|n)/.*$', gone_view), 
-    re_path(r'^category/(uttar-pradesh|kanpur-nagar|varanasi|sports|Sitapur|Shravasti|Moradabad|meerut|Rampur|chandauli|Farrukhabad|Prayagraj|Kanpur-Dehat|ambedkar-nagar|gonda|lucknow|Basti|pilibhit)/.*$', gone_view),
+    re_path(r'^(hollywood|gorakhpur-lucknow|sitapur|sambhal|prayagraj|ujjain|deoria|ghaziabad|technology|other-state|shopping|mahoba|agra|hyderabad|international|news|market|gonda|amethi|bahraich|bijnor|bollywood|Basti|Farrukhabad|kanpur-nagar|Kanpur-Dehat|gorakhpur|district|new-delhi|goa|mumbai|lucknow|national|google|delhi|kannauj|market-news|sports|varanasi|toronto-canada|mathura|mainpuri|n)/.*$', gone_view), 
+    re_path(r'^category/(Other-State|etah|Sonbhadra|kanpur|mainpuri|gorakhpur|UP-National|gautam-buddha-nagar|uttar-pradesh|kanpur-nagar|varanasi|sports|Sitapur|Shravasti|Moradabad|meerut|Rampur|chandauli|Farrukhabad|Prayagraj|Kanpur-Dehat|ambedkar-nagar|gonda|lucknow|Basti|pilibhit)/.*$', gone_view),
 ]
-
